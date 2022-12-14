82321d35b3358988498e16edd23ba10ac9465091
xeno <xeno@lv427.space>
2022-12-14 22:24:45 +0100

    created new repo for day 15

---
+++ Program.fs
@@ -0,0 +1,13 @@
+﻿open System.IO
+open System
+
+// █
+let (|Int|_|) (s:string) =
+    match Int64.TryParse s with
+    | true,value -> Some value
+    | false,_ -> None
+
+let input = File.ReadAllLines "/tmp/aoc/input" |> Array.toList
+
+
+input |> List.map (printfn "%A")    
